8c2be67b59fcabb1c1ef8eb5ac0e205d50a2038e
Orin Serimon <orin220444@protonmail.com>
2020-02-25 18:14:18 +0700

    first comiit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::io;
+fn main() {
+	loop {
+		println!("type something");
+		let mut usertypings = String::new();
+		io::stdin().read_line(&mut usertypings)
+        	.expect("Failed to read line");
+		let usertypings = usertypings.trim_end();
+		if usertypings == "quit"  {
+			println!("exiting...");
+			break;
+		}else {
+			println!("you typed: {}", &usertypings);
+		}
+	}
+}
