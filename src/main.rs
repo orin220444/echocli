d6d1bf1a36d250d7a7bebec451dc5d1f35f5b60e
Orin Serimon <orin2204444@protonmail.com>
2020-05-22 13:20:34 +0700

    format code via rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use std::io;
 fn main() {
-	loop {
-		println!("type something");
-		let mut usertypings = String::new();
-		io::stdin().read_line(&mut usertypings)
-        	.expect("Failed to read line");
-		let usertypings = usertypings.trim_end();
-		if usertypings == "quit" || usertypings == "exit" {
-			println!("exiting...");
-			break;
-		}else {
-			println!("you typed: {}", &usertypings);
-		}
-	}
+    loop {
+        println!("type something");
+        let mut usertypings = String::new();
+        io::stdin()
+            .read_line(&mut usertypings)
+            .expect("Failed to read line");
+        let usertypings = usertypings.trim_end();
+        if usertypings == "quit" || usertypings == "exit" {
+            println!("exiting...");
+            break;
+        } else {
+            println!("you typed: {}", &usertypings);
+        }
+    }
 }
